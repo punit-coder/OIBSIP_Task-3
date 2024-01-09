944c3ea5822f5b967a7b707dad1db68760bf18bf
punit-coder <153483364+punit-coder@users.noreply.github.com>
2024-01-09 22:40:02 +0530

    Add files via upload

---
+++ Task-3(Temperature Converter)/script1.js
@@ -0,0 +1,23 @@
+const convertfahrenheit = () =>{
+   var celcius = parseFloat(document.querySelector("#celcius").value);
+
+   var fahrenheitResult = (1.8 * celcius)+32;
+   var a = fahrenheitResult.toFixed(2);
+
+   document.querySelector("#celcius").value = celcius;
+   document.querySelector("#fahrenheit").value = a;
+}
+const convertcelcius = () =>{
+   var fahrenheit = parseFloat(document.querySelector("#fahrenheit").value);
+
+   var celciusResult = (fahrenheit - 32)/1.8;
+   var b = celciusResult.toFixed(2);
+
+   document.querySelector("#celcius").value = b;
+   document.querySelector("#fahrenheit").value = fahrenheit;
+}
+
+const reset = () =>{
+   document.querySelector("#celcius").value = " ";
+   document.querySelector("#fahrenheit").value = " ";
+}
